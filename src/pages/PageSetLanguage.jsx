0448bd89d28e588cfa1ca6d619a7be2f776aae00
sinri <1lliodqb@gmail.com>
2022-11-06 18:11:56 +0900

    Add Page - set language

--- src/pages/PageSetLanguage.jsx
+++ src/pages/PageSetLanguage.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import Header from "components/layout/Header"
 
@@ -13,8 +14,8 @@ const PageSetLanguage = () => {
 
 export default PageSetLanguage;
 
-
 const Popup = () => {
+  const navigate = useNavigate();
   const [language, setLanguage] = useState("KO");
   const LanguageSampleMap = {
     "KO": "안녕하세요",
@@ -22,35 +23,61 @@ const Popup = () => {
     "DE": "Halo"
   };
 
+  const naviagteToPage = () => {
+    console.log("go home")
+    navigate("/home", { replace: false })
+  }
+
   // todo: check cookie
   // todo: set cookie
 
-  return <div className="popup__wrap set-language">
-    <div className="popup__container">
-      <div className="selected-language">
-        <p>{LanguageSampleMap[language]}</p>
+  return <div className="popup-container set-language">
+    <div className="popup-wrap">
+      <div className="popup-content">
+        <div className="selected-language">
+          <p>{LanguageSampleMap[language]}</p>
+        </div>
+        <div className="list-languages-wrap">
+          <ul className="list-languages">
+            <li>
+              <button
+                className={`btn btn-lang ${language === "KO" && "--focused"}`}
+                data-language={"KO"}
+                onClick={() => setLanguage("KO")}
+              >
+                한국어
+              </button>
+            </li>
+            <li>
+              <button
+                className={`btn btn-lang ${language === "EN" && "--focused"}`}
+                data-language={"EN"}
+                onClick={() => setLanguage("EN")}
+              >
+                english
+              </button>
+            </li>
+            <li>
+              <button
+                className={`btn btn-lang ${language === "DE" && "--focused"}`}
+                data-language={"DE"}
+                onClick={() => setLanguage("DE")}
+              >
+                deutch
+              </button>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="list-languages__wrap">
-        <ul className="list-languages">
-          <li>
-            <button
-              className="btn btn-lang --focused"
-              data-language={"KO"}
-              onClick={() => setLanguage("KO")}
-            >
-              한국어
-            </button>
-          </li>
-          <li>
-            <button
-              className="btn btn-lang"
-              data-language={"EN"}
-              onClick={() => setLanguage("EN")}
-            >
-              영어
-            </button>
-          </li>
-        </ul>
+      <div className="btn-wrap">
+        <button
+          className="btn btn-close"
+          onClick={() => naviagteToPage()}
+        >닫기</button>
+        <button
+          className="btn btn-check"
+          onClick={() => naviagteToPage()}
+        >확인</button>
       </div>
     </div>
   </div>
