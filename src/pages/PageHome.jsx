4825d4af8dd2878c459e437df3e056b56060545b
sinri <1lliodqb@gmail.com>
2022-11-08 22:23:13 +0900

    Add Popup Animation

--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -8,51 +8,61 @@ import Popup from "components/Popup";
 const PageHome = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [isPopup, setIsPopup] = useState(false);
+  const [popupShutDown, setPopupshutDown] = useState(false);
   const navigate = useNavigate();
   const loadingTime = 2000;
 
   setTimeout(() => {
     setIsLoading(false);
-    setIsPopup(true);
+    // setIsPopup(true);
   }, loadingTime);
 
   const navigateToPage = (link) => {
     navigate(link);
+  };
+
+  const onClickCloseBtn = () => {
+    setPopupshutDown(!popupShutDown)
+  }
+  const onClickIcon = () => {
+    if (popupShutDown) {
+      setPopupshutDown(!popupShutDown)
+    }
   }
 
   return <div className="view home">
     <Header />
-    {isLoading && <Loading loadingTime={loadingTime} />}
+    {/* {isLoading && <Loading loadingTime={loadingTime} />} */}
     <div className="page-content">
       home
     </div>
 
-    {
-      isPopup &&
-      <Popup className="set-guide">
-        <Popup.Content>
-          <i className="image-icon-guide"></i>
-          <p className="guide-text">
-            아이돌플러스 바로가기 <br />
-            홈화면에 추가하고 편리하게 접속하세요.
-          </p>
-        </Popup.Content>
-        <Popup.Tool>
-          <button
-            className="btn btn-look-guide"
-            onClick={() => navigateToPage("guide")}
-          >
-            가이드 보기
-          </button>
-          <button
-            className="btn btn-close"
-            onClick={() => setIsPopup(false)}
-          >
-            닫기
-          </button>
-        </Popup.Tool>
-      </Popup>
-    }
+    <Popup className={popupShutDown ? "set-guide shut-down" : "set-guide"}>
+      <Popup.Content>
+        <i
+          className={`image-icon-guide ${popupShutDown && "btn"}`}
+          onClick={() => onClickIcon()}
+        ></i>
+        <p className="guide-text">
+          <strong>💜아이돌플러스 바로가기💜</strong> <br />
+          홈화면에 추가하고 편리하게 접속하세요.
+        </p>
+      </Popup.Content>
+      <Popup.Tool>
+        <button
+          className="btn btn-look-guide"
+          onClick={() => navigateToPage("guide")}
+        >
+          가이드 보기
+        </button>
+        <button
+          className="btn btn-close"
+          onClick={() => onClickCloseBtn()}
+        >
+          닫기
+        </button>
+      </Popup.Tool>
+    </Popup>
   </div>
 }
 
