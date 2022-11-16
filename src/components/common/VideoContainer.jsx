e5ba1379a6b42c258977a57e21acffff6bcff3ef
SujinJeong <sujin_jeong3@tmax.co.kr>
2022-11-16 13:28:12 +0900

    Add Category Fix

---
+++ src/components/common/VideoContainer.jsx
@@ -0,0 +1,10 @@
+const VideoItem = () => {
+  return <button className="btn btn-video-item">
+    <div className="video-wrap">
+      <video className="video-content" src=""></video>
+    </div>
+    <h4 className="video-title">video title</h4>
+  </button>
+}
+
+export default VideoItem;
