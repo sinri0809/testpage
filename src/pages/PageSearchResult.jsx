50e9fe3f962a2e64d4b785c630592cb134f32062
sinri <1lliodqb@gmail.com>
2022-11-20 22:41:55 +0900

    select style

--- src/pages/PageSearchResult.jsx
+++ src/pages/PageSearchResult.jsx
@@ -29,6 +29,17 @@ const ResultLayout = ({ input }) => {
   const resultVideo = true;
   const resultBanner = false;
 
+  const tempVideo1 = {
+    title: "abs video title video hihi"
+  }
+
+  const handleTitle = (title) => {
+    if (title.match(input)) {
+      return title.replace(input, `<b>${input}</b>`)
+    }
+  }
+
+
   return <div className="result-container">
     <div className="result-title">
       <strong className="user-input">&#34;{input}&#34;</strong>
@@ -37,29 +48,29 @@ const ResultLayout = ({ input }) => {
         <span>{"1"}</span>
       </div>
       <div className="result-sort">
-        <select name="" id="">
+        <select name="" id="" className="result-sort-select">
           <option value="">최신순</option>
           <option value="">정확순</option>
         </select>
       </div>
     </div>
     <div className="result-content">
-      {resultVideo && <ResultVideo />}
+      {resultVideo && <ResultVideo title={handleTitle(tempVideo1.title)} />}
       {resultBanner && <ResultBanner />}
     </div>
   </div>
 }
 
-const ResultVideo = () => {
+const ResultVideo = ({ title }) => {
   return <div className="result-video">
     <div className="btn-video-item">
       <div className="video-wrap">
         <video className="video-content" src=""></video>
         {/* <span className="video-total-time">{"10:11"}</span> */}
       </div>
-      <h4 className="video-title">video title</h4>
-      {/* <span className="video-date">2022.00.00</span> */}
     </div>
+    <h4 className="video-title" dangerouslySetInnerHTML={{ __html: title }}></h4>
+    {/* <span className="video-date">2022.00.00</span> */}
   </div>
 }
 
