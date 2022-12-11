51231fb500ef03ce81be40c286273328b6f02631
sinri <1lliodqb@gmail.com>
2022-12-11 17:54:15 +0900

    Add Intersection Observer Test

--- src/components/common/Video.tsx
+++ src/components/common/Video.tsx
@@ -1,5 +1,9 @@
-const Video = () => {
-  return <div className="video-wrap">
+interface VideoProps {
+  label: string;
+}
+
+const Video = ({ label }: VideoProps) => {
+  return <div className="video-wrap" data-temp={label}>
     <video className='video' src="" />
   </div>
 }
--- src/pages/PageMeetingDraft.tsx
+++ src/pages/PageMeetingDraft.tsx
@@ -1,43 +1,53 @@
-import { useState } from 'react';
+import React, { useRef, useState, useEffect, Dispatch, SetStateAction } from 'react';
 
 import Video from 'components/common/Video';
 
 const MeetingDraft = () => {
+  const users = ['user1', 'user2', 'user3', 'user4', 'user5 ', 'user6'];
 
-  const users = ['user1', 'user2', 'user3', 'user4', 'user5 ', 'user6']
+  const refVideoList = useRef<HTMLUListElement>(null);
+  const [test, setTestHorizontal] = useState(0);
+
+  const refVideoListVertical = useRef<HTMLUListElement>(null);
+  const [testVertical, setTestVertical] = useState(0);
 
   return <div className="meeting-draft">
     <Aside />
     <div className="video-list-container">
-      <ul className="video-list horizontal">
-        {users.map((user, index) =>
-          <li className='video-item' key={index}>
-            <span>{user}</span>
-            <Video />
-          </li>)
-        }
-        <li className="video-item more-infor horizontal">
-          <div className="video-wrap">
-            user
-          </div>
-        </li>
-      </ul>
-    </div>
-    <hr />
-    <div className="video-list-container">
-      <ul className="video-list vertical">
-        {users.map((user, index) =>
-          <li className='video-item' key={index}>
-            <span>{user}</span>
-            <Video />
-          </li>)
-        }
-        <li className="video-item more-infor vertical">
-          <div className="video-wrap">
-            user
-          </div>
-        </li>
-      </ul>
+      <div className="video-list-wrap horizontal">
+        <h1>horizontal example</h1>
+        hiddend user test : {test}
+        <ul className="video-list horizontal" ref={refVideoList}>
+          {users.map((user, index) =>
+            <React.Fragment key={index}>
+              <VideoItem user={user} test={test} setTest={setTestHorizontal} />
+            </React.Fragment>)
+          }
+          <li className="video-item more-infor">
+            <div className="video-wrap">
+              Hided user :
+            </div>
+          </li>
+        </ul>
+      </div>
+      <hr />
+
+      <div className="video-list-wrap vertical">
+        <h1>vertical example</h1>
+        vertical user test : {testVertical}
+        <ul className="video-list" ref={refVideoListVertical}>
+          {users.map((user, index) =>
+            <React.Fragment key={index}>
+              <VideoItem user={user} test={testVertical} setTest={setTestVertical} />
+            </React.Fragment>)
+          }
+          <li className="video-item more-infor">
+            <div className="video-wrap">
+              Hided user :
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 }
@@ -53,4 +63,52 @@ const Aside = () => {
       onClick={() => setAsideOpen(!asideOpen)}
     >{asideOpen ? "close" : "open"}</button>
   </aside>
+}
+
+interface VideoItemProps {
+  user: string;
+  test: number;
+  setTest: Dispatch<SetStateAction<number>>
+};
+
+const VideoItem = ({ user, test, setTest }: VideoItemProps) => {
+  const refVideoItem = useRef<HTMLLIElement>(null);
+
+  useEffect(() => {
+    let options = {
+      rootMargin: "0px",
+      threshold: [0.1, 0.5, 1.0]
+    };
+
+    const callback: IntersectionObserverCallback = async ([entry], observe) => {
+      if (!entry.isIntersecting) {
+        entry.target.className = `video-item hidden`;
+      } else {
+        entry.target.className = `video-item visible`;
+      }
+      await checkHiddend(entry);
+    };
+
+    const checkHiddend = async (entry: IntersectionObserverEntry) => {
+      let hidden = entry.target.parentElement?.getElementsByClassName('hidden').length;
+      if (hidden) {
+        await setTest(hidden);
+      } else if (hidden === 0) {
+        await setTest(0);
+      }
+    }
+
+    let observer: IntersectionObserver;
+    observer = new IntersectionObserver(callback, options);
+
+    if (refVideoItem.current) {
+      observer.observe(refVideoItem.current);
+    }
+    return () => {
+    }
+  }, [])
+
+  return <li className='video-item' ref={refVideoItem}>
+    <Video label={user} />
+  </li>
 }
